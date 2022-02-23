10f9d425c113cdd6daf014d8fed4cdc6b688da73
Patech_Patrice <patricedrayton86@gmail.com>
2022-02-23 16:20:11 -0500

    initial commit

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+        
+       //To create a gradiant use CAGradient Layer to create an object
+        let gradientLayer = CAGradientLayer()
+        //set layer frame to be the same as the bounds of the view controller's view
+        gradientLayer.frame = view.bounds
+        //Supply an array of colors in the order they will be displayed on the screen
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.systemOrange.cgColor,
+            UIColor.systemPink.cgColor,
+            UIColor.systemOrange.cgColor
+            
+        ]
+        
+        //Add as a sublayer to the view to allow the labels to be visible on top of the gradient background
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 
