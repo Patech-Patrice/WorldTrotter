4abefa7d2e507bc3ac409c6b1c99885f4efd600d
Patech_Patrice <patricedrayton86@gmail.com>
2022-02-24 16:51:22 -0500

    added questions and answers to quiz view controller

--- WorldTrotter/QuizViewController.swift
+++ WorldTrotter/QuizViewController.swift
@@ -5,60 +5,62 @@
 //  Created by Patrice Drayton on 2/24/22.
 //
 
+//
+//import UIKit
+//
+//class QuizViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        print("QuizViewController loaded its view")
+//    }
+//
+//}
+
+
 
 import UIKit
 
 class QuizViewController: UIViewController {
-    
+//
+    @IBOutlet var questionLabel: UILabel!
+   @IBOutlet var answerLabel: UILabel!
+
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montplier",
+        "Grapes"
+    ]
+
+    var currentQuestionIndex: Int = 0
+
+
+
+   @IBAction func showNextQuestion(_ sender: UIButton) {
+       print("Next Question Button pressed")
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+//
+   @IBAction func showAnswer(_ sender: UIButton) {
+       print("Show Answer button pressed")
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+   }
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("QuizViewController loaded its view")
+        questionLabel.text = questions[currentQuestionIndex]
     }
-    
-}
+    }
 
-
-
-//import UIKit
-//
-//class ViewController: UIViewController {
-//
-//    @IBOutlet var questionLabel: UILabel!
-//    @IBOutlet var answerLabel: UILabel!
-//
-//    let questions: [String] = [
-//        "What is 7+7?",
-//        "What is the capital of Vermont?",
-//        "What is cognac made from?"
-//    ]
-//    let answers: [String] = [
-//        "14",
-//        "Montplier",
-//        "Grapes"
-//    ]
-//
-//    var currentQuestionIndex: Int = 0
-//
-//
-//
-//    @IBAction func showNextQuestion(_ sender: UIButton) {
-//        currentQuestionIndex += 1
-//        if currentQuestionIndex == questions.count {
-//            currentQuestionIndex = 0
-//        }
-//        let question: String = questions[currentQuestionIndex]
-//        questionLabel.text = question
-//        answerLabel.text = "???"
-//    }
-//
-//    @IBAction func showAnswer(_ sender: UIButton) {
-//        let answer: String = answers[currentQuestionIndex]
-//        answerLabel.text = answer
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        questionLabel.text = questions[currentQuestionIndex]
-//    }
-//}
-//
