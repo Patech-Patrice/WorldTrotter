49bf2bb99d29f3e8fbb4d2645b64c53f8941e7a9
Patech_Patrice <patricedrayton86@gmail.com>
2022-02-24 14:55:30 -0500

    added background gradient layer to subview

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
      
         
+//       //To create a gradiant use CAGradient Layer to create an object
+//        let gradientLayer = CAGradientLayer()
+//        //set layer frame to be the same as the bounds of the view controller's view
+//        gradientLayer.frame = view.bounds
+//        //Supply an array of colors in the order they will be displayed on the screen
+//        gradientLayer.colors = [
+//            UIColor.systemPink.cgColor,
+//            UIColor.systemOrange.cgColor,
+//            UIColor.systemPink.cgColor,
+//            UIColor.systemOrange.cgColor
+//
+//        ]
+//
+//        //Add as a sublayer to the view to allow the labels to be visible on top of the gradient background
+//        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+        
+    
+    //In order to have the gradient layer stretch the full length of the screen when phone switches from portrait to landscape, do not call function when app loads. At the time your view is first created, Auto Layout has not established the size of your view for your current device (or orientation). By setting the frame in layoutSubviews you always get the latest value for the size of the view.
+    
+    override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+     
+        
        //To create a gradiant use CAGradient Layer to create an object
         let gradientLayer = CAGradientLayer()
         //set layer frame to be the same as the bounds of the view controller's view
@@ -28,7 +53,8 @@ class ViewController: UIViewController {
         ]
         
         //Add as a sublayer to the view to allow the labels to be visible on top of the gradient background
-        view.layer.insertSublayer(gradientLayer, at: 0)
+       view.layer.insertSublayer(gradientLayer, at: 0)
+       
     }
 
 
